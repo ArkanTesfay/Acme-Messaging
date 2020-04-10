4bacb5bdc8c7f54141bc6c5326cb810154656751
Arkan T Mekonen <arkantesfay333@gmail.com>
2020-04-10 14:15:12 -0700

    changed delete route to get because it wasn't working on production

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "login", to: "session#new"
   post "login", to:"session#create"
 
-  delete "logout", to: "session#destroy"
+  get "logout", to: "session#destroy"
 
   get  'chatroom', to:"chatroom#index"
   post 'message', to:"messages#create"
